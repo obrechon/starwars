8e5ea2f2bdebaa27f5facfe3ae711bf748177e54
obrechon <olivier.brechon@dataiku.com>
2026-01-05 14:34:26 +0100

    1 handle only

--- CustomNode.jsx
+++ CustomNode.jsx
@@ -1,14 +1,17 @@
 import React, { memo } from 'react';
 import { Handle, Position } from '@xyflow/react';
 
-export default memo(({ data }) => {
+export default memo(({ data = {} }) => {
+  // Default to all handles if `data.handles` is not provided
+  const { label, handles = { top: true, right: true, bottom: true, left: true } } = data;
+
   return (
     <>
-      {data.label}
-      <Handle type="source" position={Position.Top} id="a" />
-      <Handle type="source" position={Position.Right} id="b" />
-      <Handle type="source" position={Position.Bottom} id="c" />
-      <Handle type="source" position={Position.Left} id="d" />
+      {label}
+      {handles.top && <Handle type="source" position={Position.Top} id="a" />}
+      {handles.right && <Handle type="source" position={Position.Right} id="b" />}
+      {handles.bottom && <Handle type="source" position={Position.Bottom} id="c" />}
+      {handles.left && <Handle type="source" position={Position.Left} id="d" />}
     </>
   );
 });
