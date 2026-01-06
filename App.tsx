e8b71140782c0895eb47e57ded7f6f8361dfb395
obrechon <olivier.brechon@dataiku.com>
2026-01-06 10:21:59 +0100

    Universe UI

--- App.tsx
+++ App.tsx
@@ -34,26 +34,26 @@ const edgeTypes = {
 const initialNodes: Node[] = [
   {
     id: '1',
-    position: { x: -200, y: 0 },
-    data: { label: 'Tatooine', handles: { right: true } },
+    position: { x: -300, y: 0 },
+    data: { label: 'Tatooine', handles: { right: true }, style: { width: 100, height: 100, backgroundColor: '#F2D7C3', color: '#886362'} },
     type: 'custom'
   },
   {
     id: '2',
-    position: { x: -100, y: -200 },
-    data: { label: 'Dagobah', handles: { bottom: true } },
+    position: { x: -150, y: -300 },
+    data: { label: 'Dagobah', handles: { bottom: true }, style: { width: 220, height: 220, backgroundColor: '#B5C2BE', color: '#202E2E'} },
     type: 'custom',
   },
   {
     id: '3',
-    position: { x: 300, y: -200 },
-    data: { label: 'Endor', handles: { left: true } },
+    position: { x: 400, y: -200 },
+    data: { label: 'Endor', handles: { left: true }, style: { width: 280, height: 280, backgroundColor: '#A1CEC6', color: '#48615F'} },
     type: 'custom',
   },
   {
     id: '4',
     position: { x: 100, y: 200 },
-    data: { label: 'Hoth', handles: { top: true } },
+    data: { label: 'Hoth', handles: { top: true }, style: { width: 75, height: 75, backgroundColor: '#F5FAFF', color: '#45A3A4'} },
     type: 'custom',
   },
 ];
@@ -91,6 +91,7 @@ const createTravelEdge = (sourceNode: Node, targetNode: Node, sourceHandle: stri
     targetHandle,
     deletable: false,
     reconnectable: false,
+    selectable: false,
     data: { label: `${travelTime} days` },
     type: 'custom',
   };
--- CustomNode.jsx
+++ CustomNode.jsx
@@ -1,17 +1,31 @@
 import React, { memo } from 'react';
 import { Handle, Position } from '@xyflow/react';
 
+const nodeStyle = {
+    backgroundColor: '#fff',
+    width: 50,
+    height: 50,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: '100%',
+    color: 'black'
+};
+
 export default memo(({ data = {} }) => {
   // Default to all handles if `data.handles` is not provided
-  const { label, handles = { top: true, right: true, bottom: true, left: true } } = data;
+  const { label, handles = { top: true, right: true, bottom: true, left: true }, style } = data;
+
+  const mergedStyles = { ...nodeStyle, ...style };
 
   return (
-    <>
+    <div style={mergedStyles}>
       {label}
       {handles.top && <Handle type="source" position={Position.Top} id="a" />}
       {handles.right && <Handle type="source" position={Position.Right} id="b" />}
       {handles.bottom && <Handle type="source" position={Position.Bottom} id="c" />}
       {handles.left && <Handle type="source" position={Position.Left} id="d" />}
-    </>
+    </div>
   );
 });
